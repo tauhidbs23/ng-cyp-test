38b017f5217df4ae4cd9f6ece14902ebde563b45
tauhidbs23 <tauhidul.islam@brainstation-23.com>
2022-09-08 17:42:20 +0600

    users crud working perfectly

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {UsersService} from "../services/users/users.service";
 
 @Component({
@@ -10,14 +10,42 @@ import {UsersService} from "../services/users/users.service";
 })
 export class UserFormComponent implements OnInit {
   form: FormGroup;
+  userDetails: any;
+  userId: any;
 
   constructor(private formBuilder: FormBuilder,
               private router: Router,
+              private activatedRoute: ActivatedRoute,
               private usersService: UsersService) {
   }
 
   ngOnInit(): void {
     this.initForm();
+
+    this.activatedRoute.params.subscribe(paramsId => {
+      if (paramsId) {
+        this.userId = paramsId;
+        this.getSingleUser(paramsId['id']);
+      }
+    });
+
+    // if (this.userDetails) {
+    //   const {name, username, email, phone, website} = this.userDetails;
+    //   console.log(name, username, email, phone, website);
+    //   this.form.patchValue({name, username, email, phone, website})
+    // }
+
+  }
+
+  getSingleUser(id: string): void {
+    this.usersService.getUserById(id).subscribe(res => {
+      this.userDetails = res;
+      if (this.userDetails) {
+        const {name, username, email, phone, website} = this.userDetails;
+        console.log(name, username, email, phone, website);
+        this.form.patchValue({name, username, email, phone, website})
+      }
+    });
   }
 
   initForm(): void {
@@ -34,6 +62,16 @@ export class UserFormComponent implements OnInit {
     if (this.form.value) {
       this.usersService.addUser(this.form.value).subscribe(res => {
         console.log('user created');
+        this.router.navigate(['/users'], {replaceUrl: true});
+      })
+    }
+  }
+
+  updateUser(): void {
+    if (this.form.value) {
+      console.log('this.userId ->', this.userId?.id);
+      this.usersService.editUser(this.userId?.id, this.form.value).subscribe(res => {
+        console.log('update info updated successful');
         this.router.navigate(['/users'], {replaceUrl: true});
       })
     }
