ba54b98896e91c3df8ce2e9098e016dd65a85fef
tauhidbs23 <tauhidul.islam@brainstation-23.com>
2022-09-08 20:55:43 +0600

    e-to-e test working fine

--- cypress/e2e/users.cy.ts
+++ cypress/e2e/users.cy.ts
@@ -1,13 +1,24 @@
-describe('Users', () => {
+describe('Users Page:', () => {
   it('should visit User page', () => {
     cy.visit('/users');
   });
 
   it('should have users works', () => {
-    cy.contains('users works!')
-    // cy.get("a[href='/adventure/1']").click();
-    // cy.get('#title').should('have.text', 'Breithorn');
-
+    cy.contains('Users List');
   });
+
+  it('should display the logo', ()=>{
+    cy.get('h1').should('have.text','BS23-USER-LIST');
+  })
+
+
+  it('should visit user details page', () => {
+    cy.visit('/users/details/1');
+  });
+
+  it('should have users details', () => {
+    cy.get('h2').should('have.text','Users Details');
+  });
+
 
 });
